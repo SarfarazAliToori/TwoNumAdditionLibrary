34f797cba198a7e6d8ef582caa460cdbb7973bc6
faraznb <alifarazzain@gmail.com>
2021-12-10 09:55:15 +0500

    interface added

---
+++ Addition/src/main/java/com/example/addition/OnClickAddition.kt
@@ -0,0 +1,12 @@
+package com.example.addition
+
+interface OnClickAddition {
+
+    fun twoNumAddition(a: Int, b: Int)
+
+    fun threeNumAddition(a: Int, b: Int, c: Int)
+
+    fun twoNumberMultiplication(a: Int, b: Int)
+
+    fun twoNumberDivision(a: Int, b: Int)
+}
